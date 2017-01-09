fefedbe5809a78e63e4ef8bbd6ab09278aff6f90
thinkbaek <mostcomfort@naver.com>
2017-01-09 11:11:01 +0900

    AddStarterKitProject

---
+++ SketchPrototypeStarterKit/SketchPrototypeStarterKit/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SketchPrototypeStarterKit
+//
+//  Created by thinkBAEK on 2017. 1. 9..
+//  Copyright © 2017년 codershigh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
