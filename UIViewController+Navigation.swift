84df3d1a3ce9c049435bbf95985ad62858e669b8
Lee Jaehoon <wannabewize@gmail.com>
2017-08-21 21:57:31 +0900

    이미지 피커 선택 후 태그 100번 이미지 뷰에 반영

--- UIViewController+Navigation.swift
+++ UIViewController+Navigation.swift
@@ -7,19 +7,39 @@
 //
 
 import UIKit
-import ObjectiveC
 
 private var backButtonHidden : Bool = false
 private var tapKBDismiss : Bool = false
 
+private let ImageViewTag = 100
+
+class ImagePickerDelegate : NSObject, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    weak var imageView: UIImageView?
+    public func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        
+        let originalImage = info[UIImagePickerControllerOriginalImage] as! UIImage
+        let editedImage = info[UIImagePickerControllerEditedImage] as? UIImage
+        imageView?.image = editedImage ?? originalImage
+        picker.dismiss(animated: true, completion: nil)
+        imagePickerDelegate = nil
+    }
+    
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        picker.dismiss(animated: true, completion: nil)
+        imagePickerDelegate = nil
+    }
+}
+
+var imagePickerDelegate: ImagePickerDelegate?
+
 
 public extension UIViewController {
     
-    @IBAction func modalDismiss(_ sender : AnyObject){
+    @IBAction func modalDismiss(_ sender : Any){
         self.dismiss(animated: true, completion: nil)
     }
     
-    @IBAction func modalDismissPush(_ sender : AnyObject){
+    @IBAction func modalDismissPush(_ sender : Any){
         var destVC : UIViewController! = nil
         if let presentingVC = self.presentingViewController as? UITabBarController {
             if let tempVC = presentingVC.selectedViewController as? UINavigationController {
@@ -37,29 +57,32 @@ public extension UIViewController {
         self.dismiss(animated: true, completion: nil)
     }
     
-    
-    
-    @IBAction func navigationBack(_ sender : AnyObject){
-        if let navigationCongtroller = self.navigationController {
-            navigationController?.popViewController(animated: true)
+    @IBAction func navigationBack(_ sender : Any){
+        if let navi = self.navigationController {
+            navi.popViewController(animated: true)
         }
     }
     
-    @IBAction func navigationBackToRoot(_ sender : AnyObject){
-        if let navigationCongtroller = self.navigationController {
-            navigationController?.popToRootViewController(animated: true)
+    @IBAction func navigationBackToRoot(_ sender : Any){
+        if let navi = self.navigationController {
+            navi.popToRootViewController(animated: true)
         }
     }
     
-    @IBAction func keyboardDismiss(_ sender: AnyObject) {
+    @IBAction func keyboardDismiss(_ sender: Any) {
         for view in self.view.subviews {
             view.resignFirstResponder()
         }
     }
     
-    @IBAction func openPhotoLibrary(_ sender: AnyObject) {
+    
+    
+    @IBAction func openPhotoLibrary(_ sender: Any) {
+        imagePickerDelegate = ImagePickerDelegate()
+        imagePickerDelegate?.imageView = self.view.viewWithTag(ImageViewTag) as? UIImageView
         
         let imagePickerController = UIImagePickerController()
+        imagePickerController.delegate = imagePickerDelegate!
         imagePickerController.sourceType = UIImagePickerControllerSourceType.savedPhotosAlbum
         imagePickerController.allowsEditing = true
         self.present(imagePickerController, animated: true, completion: { imageP in
@@ -77,19 +100,4 @@ public extension UIViewController {
     }
     
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
